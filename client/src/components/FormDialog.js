6782d5e50fe1f59a60c26ba09da122f4c9949a95
chstrcpprpt <cjhallen@gmail.com>
2020-03-28 16:51:19 +1100

    Added modal component to navbar

--- client/src/components/FormDialog.js
+++ client/src/components/FormDialog.js
@@ -8,29 +8,74 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import AddCircleRoundedIcon from '@material-ui/icons/AddCircleRounded';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
 
 export default function FormDialog() {
   //modal state
   const [open, setOpen] = React.useState(false);
   //form state
-  const [value, setValue] = React.useState('');
+  const [section, setSection] = React.useState('');
+  //item state
+  const [item, setItem] = React.useState('');
 
+  //handle new item entered in form
+  const handleItemChange = event => {
+    console.log(`Item: ${event.target.value}`);
+    setItem(event.target.value);
+  };
+
+  //handle new section entered in form
+  const handleSectionChange = event => {
+    console.log(`Section: ${event.target.value}`);
+    setSection(event.target.value);
+  };
+
+  //handle open & close modal
   const handleClickOpen = () => {
     setOpen(true);
   };
-
   const handleClose = () => {
     setOpen(false);
   };
 
-  const handleChange = event => {
-    setValue(event.target.value);
-  };
+  //sections for form
+  const sections = [
+    {
+      value: 'Deli',
+      label: 'Deli',
+    },
+    {
+      value: 'Fridge',
+      label: 'Fridge',
+    },
+    {
+      value: 'Frozen',
+      label: 'Frozen',
+    },
+    {
+      value: 'Fruit & Veg',
+      label: 'Fruit & Veg',
+    },
+    {
+      value: 'Household',
+      label: 'Household',
+    },
+    {
+      value: 'Meats',
+      label: 'Meats',
+    },
+    {
+      value: 'Pantry',
+      label: 'Pantry',
+    },
+    {
+      value: 'Personal Care',
+      label: 'Personal Care',
+    },
+    {
+      value: 'Other',
+      label: 'Other',
+    },
+  ];
 
   return (
     <div>
@@ -45,25 +90,35 @@ export default function FormDialog() {
             Add your new grocery item
           </DialogContentText>
 
-          <TextField
-            autoFocus
-            margin="dense"
-            id="item"
-            label="Item"
-            type="text"
-            fullWidth
-          />
-
-          <FormControl component="fieldset">
-            <FormLabel component="legend">Section</FormLabel>
-            <RadioGroup aria-label="section" name="section1" value={value} onChange={handleChange}>
-              <FormControlLabel value="Fridge" control={<Radio />} label="Fridge" />
-              <FormControlLabel value="Fruit & Veg" control={<Radio />} label="Fruit & Veg" />
-              <FormControlLabel value="Bakery" control={<Radio />} label="Bakery" />
-              <FormControlLabel value="Meats" control={<Radio />} label="Meats" />
-              <FormControlLabel value="Other" control={<Radio />} label="Other" />
-            </RadioGroup>
-          </FormControl>
+            <form className="" noValidate autoComplete="off">
+              <TextField
+                  autoFocus
+                  margin="dense"
+                  id="item"
+                  label="Item"
+                  type="text"
+                  onChange={handleItemChange}
+                  value={item}
+                  fullWidth
+                />
+              <TextField
+                id="section"
+                select
+                label="Section"
+                value={section}
+                onChange={handleSectionChange}
+                SelectProps={{
+                  native: true,
+                }}
+                helperText="Choose a section"
+              >
+                {sections.map(option => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </TextField>
+            </form>
 
         </DialogContent>
         <DialogActions>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -29,7 +29,7 @@ export default function ButtonAppBar() {
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h3" className={classes.title}>
             groceries
           </Typography>
           <FormDialog />
