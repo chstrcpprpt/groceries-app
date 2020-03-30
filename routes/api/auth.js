2d9cb95b7ffd92d270c56068536d5968a07c1bf7
chstrcpprpt <cjhallen@gmail.com>
2020-03-30 13:40:15 +1100

    added route to get user from token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const config = require("config");
 const jwt = require("jsonwebtoken");
+const auth = require("../../middleware/auth");
 
 const User = require("../../models/User");
 
@@ -55,6 +56,15 @@ router.post("/", (req, res) => {
           )
         });
 
+    });
+});
+
+//GET - get user data || private
+router.get("/user", auth, (req, res) => {
+  User.findById(req.user.id)
+    .select("-password")
+    .then(user => {
+      res.json(user)
     });
 });
 
